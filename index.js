20617da0626ee4e6ba26fea5e07d82a1ebaf2425
ulchatur <34676077+ulchatur@users.noreply.github.com>
2020-12-14 17:37:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
+var favicon = require('serve-favicon');
 var app = express();
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 app.get('/', function (req, res) {
   res.send('Hello Airbus!');
